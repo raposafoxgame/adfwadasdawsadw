d70234aa57657f5f86b7f0f7f44f2ab0c64fa441
raposafoxgame <60119158+raposafoxgame@users.noreply.github.com>
2023-10-03 18:11:54 -0300

    Update obfuscated (3).lua

--- obfuscated (3).lua
+++ obfuscated (3).lua
@@ -1,3 +1,21 @@
+
+local url_to_use = "https://cc-checker-foxseven.000webhostapp.com/Whitelist/"
+
+local random_number = math.random(500,2000)
+local current_time = os.time()
+local response = game:HttpGet(url_to_use.."main.php?condition=checkwhitelist&key="..key.."&time="..tostring(current_time).."&n="..tostring(random_number))
+
+if tonumber(response) == current_time*random_number then
+    game:GetService("Players").LocalPlayer:Kick("Invalid Key")
+    return
+end
+if tonumber(response) ~= current_time-random_number then
+    game:GetService("Players").LocalPlayer:Kick("You must wait "..response.." seconds before using on a new ip!")
+    return
+end
+
+print("Whitelisted Woohooooo")
+
 if game.PlaceId == 13772394625 then
 
 local function startAutoParry()
